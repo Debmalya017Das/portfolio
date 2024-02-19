93ae5a8c14ca783c579d1fab3d6469a71f50d3e5
Debmalya017Das <106435168+Debmalya017Das@users.noreply.github.com>
2024-02-19 13:31:31 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,11 +7,27 @@ document.addEventListener('DOMContentLoaded', function () {
         body.classList.toggle('dark-mode');
         const isDarkMode = body.classList.contains('dark-mode');
         localStorage.setItem('dark-mode', isDarkMode);
+
+        // Toggle the logo when dark mode is toggled
+        const logo = document.querySelector('.logo');
+        if (isDarkMode) {
+            // Change to sun icon when in dark mode
+            logo.classList.add('fa-sun');
+            logo.classList.remove('fa-moon');
+        } else {
+            // Change back to moon icon when not in dark mode
+            logo.classList.add('fa-moon');
+            logo.classList.remove('fa-sun');
+        }
     });
 
     // Check if dark mode was previously set
     const isDarkModeStored = localStorage.getItem('dark-mode') === 'true';
     if (isDarkModeStored) {
         body.classList.add('dark-mode');
+        // Change the logo if dark mode was previously set
+        const logo = document.querySelector('.logo');
+        logo.classList.add('fa-sun');
+        logo.classList.remove('fa-moon');
     }
 });
