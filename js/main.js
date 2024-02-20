f6683f1912528340eac426e6f26f7433459ec031
Debmalya017Das <106435168+Debmalya017Das@users.noreply.github.com>
2024-02-20 23:38:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -122,23 +122,77 @@
     });
 
     
-    document.addEventListener('DOMContentLoaded', function () {
-        const toggleDarkModeBtn = document.getElementById('toggle-dark-mode');
-        const body = document.body;
+//     document.addEventListener('DOMContentLoaded', function () {
+//         const toggleDarkModeBtn = document.getElementById('toggle-dark-mode');
+//         const body = document.body;
     
-        toggleDarkModeBtn.addEventListener('click', function () {
-            body.classList.toggle('dark-mode');
-            const isDarkMode = body.classList.contains('dark-mode');
-            localStorage.setItem('dark-mode', isDarkMode);
-        });
+//         toggleDarkModeBtn.addEventListener('click', function () {
+//             body.classList.toggle('dark-mode');
+//             const isDarkMode = body.classList.contains('dark-mode');
+//             localStorage.setItem('dark-mode', isDarkMode);
+//         });
     
-        // Check if dark mode was previously set
-        const isDarkModeStored = localStorage.getItem('dark-mode') === 'true';
-        if (isDarkModeStored) {
-            body.classList.add('dark-mode');
-        }
+//         // Check if dark mode was previously set
+//         const isDarkModeStored = localStorage.getItem('dark-mode') === 'true';
+//         if (isDarkModeStored) {
+//             body.classList.add('dark-mode');
+//         }
+//     });
+
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBvCl0M6S_y0jYL3dvRmZeXii3lj80oBOo",
+        authDomain: "contact-form-ed7a8.firebaseapp.com",
+        databaseURL: "https://contact-form-ed7a8-default-rtdb.firebaseio.com",
+        projectId: "contact-form-ed7a8",
+        storageBucket: "contact-form-ed7a8.appspot.com",
+        messagingSenderId: "429530716576",
+        appId: "1:429530716576:web:d34e136a0484e5940cecc5",
+        measurementId: "G-239HHF29RM"
+      };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+
+var messageref = firebase.database().ref('con');
+
+
+
+document.getElementById('con').addEventListener('submit',send);
+
+// submit form 
+function send(e){
+    e.preventDefault();
+
+    // console.log(123);
+    var name = getval( "name" );
+    var email =getval('email');
+    var subject = getval('subject');
+    var message = getval('message');
+
+    // console.log(name);
+    saveMessage(name ,email, subject ,message);
+}
+
+// function to get form values 
+
+// function getval(id){
+//     return document.getElementById(id).value;
+// }
+
+// save them
+function save(name,email,subject,message){
+    var newmessage = messageref.push();
+
+    newmessage.set({
+        name:name,
+        email:email,
+        subject:subject,
+        message:message,
     });
+}
     
     
 })(jQuery);
 
+// listen from form 
