92af5feee728f230bebf6263690355d9d96bce52
Debmalya017Das <106435168+Debmalya017Das@users.noreply.github.com>
2024-02-22 18:05:56 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -40,3 +40,4 @@ function send(e){
 
     console.log(123);
 }
+
--- js/main.js
+++ js/main.js
@@ -141,6 +141,27 @@
 
 
 
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 20,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:3
+                }
+            },
+            touchDrag: true,
+            mouseDrag: true
+        });
+    });
+
+
     
 })(jQuery);
 
